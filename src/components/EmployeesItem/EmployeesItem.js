f6f611a84540da564701dcdfdbaa1b8a890a7e6d
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-02-17 01:09:31 +0200

    add click on name and click on cookie

--- src/components/EmployeesItem/EmployeesItem.js
+++ src/components/EmployeesItem/EmployeesItem.js
@@ -1,19 +1,33 @@
-import { FaCookie, FaTrash } from 'react-icons/fa';
+import { useState } from 'react';
+import { FaCookie, FaTrash, FaStar } from 'react-icons/fa';
 import './EmployeesItem.css';
 
 const EmployeesItem = ({ name, salary, increase }) => {
+  const [isIncrease, setIsIncrease] = useState(increase);
+  const [liked, setLiked] = useState(false);
+
+  const handleIncrease = () => {
+    setIsIncrease(prevState => !prevState);
+  };
+
+  const handleClick = () => {
+    setLiked(prevState => !prevState);
+  };
+
   const itemClassNames = [
     'list-group-item',
     'd-flex',
     'justify-content-between',
     'align-items-center',
   ];
-
-  if (increase) itemClassNames.push('increase');
+  if (isIncrease) itemClassNames.push('increase');
+  if (liked) itemClassNames.push('like');
 
   return (
     <li className={itemClassNames.join(' ')}>
-      <span className="list-group-item-label">{name}</span>
+      <span className="list-group-item-label" onClick={handleClick}>
+        {name}
+      </span>
       <input
         type="text"
         className="list-group-item-input"
@@ -23,6 +37,7 @@ const EmployeesItem = ({ name, salary, increase }) => {
         <button
           type="button"
           className="btn-cookie btn-sm d-flex justify-content-center align-items-center"
+          onClick={handleIncrease}
         >
           <FaCookie className="fas fa-cookie" />
         </button>
@@ -32,6 +47,7 @@ const EmployeesItem = ({ name, salary, increase }) => {
         >
           <FaTrash className="fas fa-trash" />
         </button>
+        <FaStar className="fas fa-star" />
       </div>
     </li>
   );
