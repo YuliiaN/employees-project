a356367e31d132805806f02d295caba15c623ec4
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-02-17 18:35:56 +0200

    fix tabs styles

--- src/components/NavTabs/NavTabs.js
+++ src/components/NavTabs/NavTabs.js
@@ -3,15 +3,21 @@ import Tab from 'react-bootstrap/Tab';
 import './NavTabs.css';
 
 const NavTabs = ({ handleTabs }) => {
+  const tabsData = [
+    { title: 'Всі співробітники', key: '1' },
+    { title: 'На підвищення', key: '2' },
+    { title: 'З/П більше 1000$', key: '3' },
+  ];
+
   const handleSelect = key => {
     handleTabs(key);
   };
 
   return (
     <Tabs defaultActiveKey={'1'} fill transition={true} onSelect={handleSelect}>
-      <Tab title="Всі співробітники" eventKey={'1'} />
-      <Tab title="На підвищення" eventKey={'2'} />
-      <Tab title="З/П більше 1000$" eventKey={'3'} />
+      {tabsData.map(tab => (
+        <Tab title={tab.title} eventKey={tab.key} />
+      ))}
     </Tabs>
   );
 };
